bee1c59598a0824da90680b9ba1c57ea080a7fbf
luismarcodev <luismarco.web@gmail.com>
2022-03-10 13:39:33 +0100

    segundo de la rama luis

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-echo "manolo-benifaionero";
+echo "manolo-benifaionero oh";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
