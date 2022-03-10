06eb6a3b9d297cad9901f27a3fbc10a552c6fc5f
luismarcodev <luismarco.web@gmail.com>
2022-03-10 13:35:17 +0100

    primero de la rama luis

--- public/index.php
+++ public/index.php
@@ -4,7 +4,7 @@ use Illuminate\Contracts\Http\Kernel;
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-
+echo "manolo-benifaionero";
 /*
 |--------------------------------------------------------------------------
 | Check If The Application Is Under Maintenance
